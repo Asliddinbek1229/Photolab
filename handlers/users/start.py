9114d98b07097ad9b6507927e85d9ccac23d97d4
Asliddinbek1229 <asliddinbek1229@gmail.com>
2023-10-17 14:31:36 +0500

    DataBase

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,7 +11,30 @@ from loader import dp, bot
 
 from keyboards.default.menuKeyvboard import menuKeyboard
 
-users = {}
+users = {982935447: ['Asliddin 🇪🇭', 2],
+         5118894186: ['Kompyuter xizmatlari', 1],
+         1339330585: ['SADULLOH', 2],
+         6399246532: ['💞💞', 2],
+         855997578: ['Nodiraxon Bozorboyevna😊🙂 Axmadaliyeva', 0],
+         569719079: ['С. Б. Ахмедова 22', 0],
+         5357920886: ['•™•', 2],
+         5102556968: ['Sohib Kuvayev', 2],
+         5997942326: ['Abdumuxtor', 0],
+         818234562: ['™TM™', 2],
+         5728922975: ['Sh....', 2],
+         1429936437: ['Samandar', 2],
+         2121270055: ['⚜️HUMOYUN⚜️', 0],
+         1483757455: ['Мунира Курбонова', 0],
+         6199861108: ['Qalandarova Dilrabo', 2],
+         493981667: ['𝓓𝓲𝓵𝓼𝓱𝓸𝓭𝓫𝓮𝓴 𝓨𝓪𝓺𝓾𝓫𝓫𝓸𝔂𝓮𝓿', 1],
+         5761631472: ['@Eldor_Raxmonov', 2],
+         2062270180: ['☝️МashaAlloh👆', 2],
+         5985068114: ['꧁H҉A҉C҉K҉E҉R҉꧂', 2],
+         307959200: ['Shokirjon Yulchibayev', 1],
+         2122164437: ['꯭➊⃘⃝꯭➣⃘⃝꯭🕋𝚳꯭꯭𝚰꯭꯭𝐑𝐉꯭꯭𝚨꯭꯭𝐋꯭꯭𝚯꯭꯭𝐋꯭꯭➤⃝⃚⃖❤️', 1],
+         972609448: ['Zuhriddin', 1],
+         6674571198: ['Isfandiyor Xolbekov', 0]
+         }
 
 @dp.message_handler(Command('start'), state='*')
 async def show_channels(message: types.Message):
